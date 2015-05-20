e2dfceca8fd8b07eaa9fa06ae260989d8f9f6540
Christian Neff <christian.neff@gmail.com>
2015-05-20 19:51:12 +0200

    Added the LogStorage class

---
+++ lib/Storage/LogStorage.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Gatekeeper Library
+ * Copyright (C) 2015 IceFlame.net
+ *
+ * Permission to use, copy, modify, and/or distribute this software for
+ * any purpose with or without fee is hereby granted, provided that the
+ * above copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE
+ * FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY
+ * DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER
+ * IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
+ * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * @package  FlameCore\Gatekeeper
+ * @version  0.1-dev
+ * @link     http://www.flamecore.org
+ * @license  ISC License <http://opensource.org/licenses/ISC>
+ */
+
+namespace FlameCore\Gatekeeper\Storage;
+
+use FlameCore\Gatekeeper\Result\PositiveResult;
+use FlameCore\Gatekeeper\Result\ResultInterface;
+use FlameCore\Gatekeeper\Visitor;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
+use Psr\Log\LoggerInterface;
+
+/**
+ * A basic Storage that writes to a Logger.
+ *
+ * @author   Christian Neff <christian.neff@gmail.com>
+ */
+class LogStorage implements StorageInterface, LoggerAwareInterface
+{
+    use LoggerAwareTrait;
+
+    /**
+     * @param \Psr\Log\LoggerInterface $logger
+     */
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->setLogger($logger);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function insert(Visitor $visitor, ResultInterface $result)
+    {
+        $explanation = $result->getExplanation();
+        $message = $explanation['logtext'];
+        $context = $visitor->toArray();
+
+        if ($result instanceof PositiveResult) {
+            $this->logPositiveResult($message, $context);
+        } else {
+            $this->logNegativeResult($message, $context);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function count()
+    {
+        return -1;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function countBlocked()
+    {
+        return -1;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function cleanup()
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function optimize()
+    {
+        return true;
+    }
+
+    /**
+     * @param string $message
+     * @param array $context
+     */
+    protected function logPositiveResult($message, array $context)
+    {
+        $this->logger->notice($message, $context);
+    }
+
+    /**
+     * @param string $message
+     * @param array $context
+     */
+    protected function logNegativeResult($message, array $context)
+    {
+        $this->logger->info($message, $context);
+    }
+}
